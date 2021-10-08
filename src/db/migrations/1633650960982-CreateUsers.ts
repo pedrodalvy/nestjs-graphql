ae50d17364cdcceae7e3114538b7844345f97270
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-07 20:11:51 -0400

    feat: add CreateUsers migration

---
+++ src/db/migrations/1633650960982-CreateUsers.ts
@@ -0,0 +1,43 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class CreateUsers1633650960982 implements MigrationInterface {
+  private table = new Table({
+    name: 'users',
+    columns: [
+      {
+        name: 'id',
+        type: 'INTEGER',
+        isPrimary: true,
+        isGenerated: true,
+        generationStrategy: 'increment',
+      },
+      {
+        name: 'email',
+        type: 'varchar',
+        length: '255',
+        isUnique: true,
+        isNullable: false,
+      },
+      {
+        name: 'created_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+      {
+        name: 'updated_at',
+        type: 'timestamptz',
+        isNullable: false,
+        default: 'now()',
+      },
+    ],
+  });
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(this.table);
+  }
+}
