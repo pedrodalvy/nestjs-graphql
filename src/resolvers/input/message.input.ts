5153464e2822cf42baaaa909e2b2fa4d8a7f0356
Pedro Dalvy <pedrodalvy@outlook.com>
2021-10-08 13:40:28 -0400

    refact: remove comments

--- src/resolvers/input/message.input.ts
+++ src/resolvers/input/message.input.ts
@@ -1,29 +1,4 @@
 import { Field, InputType } from '@nestjs/graphql';
-// import UserInput from './user.input';
-
-// @InputType()
-// class MessageUserConnectInput {
-//   @Field()
-//   readonly id: number;
-// }
-//
-// @InputType()
-// class MessageUserInput {
-//   @Field({ nullable: true })
-//   readonly connect: MessageUserConnectInput;
-//
-//   @Field({ nullable: true })
-//   readonly create: UserInput;
-// }
-//
-// @InputType()
-// class MessageInput {
-//   @Field()
-//   readonly content: string;
-//
-//   @Field()
-//   readonly user: MessageUserInput;
-// }
 
 @InputType()
 class MessageInput {
